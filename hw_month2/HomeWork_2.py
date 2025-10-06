c1a4f84169260320d8e5e65407e882bfa3e8162c
nuruyql <nurbolsuiunbekov626@gmail.com>
2025-10-06 17:04:26 +0600

    did second hw

--- hw_month2/HomeWork_2.py
+++ hw_month2/HomeWork_2.py
@@ -1,39 +1,45 @@
+
+from dataclasses import dataclass
+
+
+@dataclass
 class Person:
-    def __init__(self, name, age, occupation):
-        self.name = name
-        self.age = age
-        self.occupation = occupation
+    name: str
+    occupation: str
 
     def introduce(self):
-        return f"Hello, my name is {self.name}, I am {self.age} years old and I work as a {self.occupation}"
-
-
-class ClassMate(Person):
-    def __init__(self, name, age, occupation, group_name):
-        super().__init__(name, age, occupation)
+        return (f"My name is {self.name},"
+                f"i work as {self.occupation}"
+                )
+class Classmate(Person):
+    def __init__(self,name,occupation,group_name):
+        super().__init__(name,occupation)
         self.group_name = group_name
-
     def introduce(self):
-        return f"{super().introduce()} and I am in group '{self.group_name}'."
+        return f"{super().introduce()} and my group is {self.group_name}"
 
 
 class Friend(Person):
-    def __init__(self, name, age, occupation, hobby):
-        super().__init__(name, age, occupation)
+    def __init__(self,name,occupation,hobby):
+        super().__init__(name,occupation)
         self.hobby = hobby
-
     def introduce(self):
-        return f"{super().introduce()} My hobby is {self.hobby}."
+        return f"{super().introduce()} and my hobby is {self.hobby}"
+class BestFriend(Friend):
+    def __init__(self,name,occupation,hobby,shared_memory):
+        super().__init__(name,occupation,hobby)
+        self.shared_memory = shared_memory
+    def introduce(self):
+        return f"{super().introduce()} and i remember about our {self.shared_memory}"
 
 
-# Создаем объекты
-person1 = Person("Nurbol", 17, "Backend developer")
-person2 = ClassMate("Max", 17, "Frontend developer", "CS-101")
-person3 = Friend("Timur", 17, "Security developer", "Basketball")
 
-# Список всех объектов
-people = [person1, person2, person3]
 
-# Вызываем introduce для каждого объекта через цикл
-for p in people:
-    print(p.introduce())
+classmate1 = Classmate("Nurbol","Backend Developer","CS24-12")
+classmate2 = Classmate("Aktan","Frontend Developer","CS24-14")
+friend1 = Friend("Maxim","Frontend Developer","skiing")
+friend2 = Friend("Timur","Backend Developer","mobile games")
+best_friend = BestFriend("Muhammed","Dentist","football","first meeting")
+introduce_object = [classmate1,classmate2,friend2,friend1,best_friend]
+for i in introduce_object:
+    print(i.introduce())
