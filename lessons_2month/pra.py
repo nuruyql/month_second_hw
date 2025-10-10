21532cfc426e41ab69361460152730e26053aed8
nuruyql <nurbolsuiunbekov626@gmail.com>
2025-10-10 14:32:53 +0600

    3 hw

--- lessons_2month/pra.py
+++ lessons_2month/pra.py
@@ -32,3 +32,66 @@
 #     return num >0 and (num & (num -1))==0
 # print(degree(4))
 
+# def twoN(nums,target):
+#     known = {}
+#
+#     for i, num in enumerate(nums):
+#         need = target - num
+#         if need in known:
+#             return [known[need],i]
+#         known[num]=i
+#
+#
+#
+#
+# print(twoN([1,2,34,5,6,7,8],36))
+
+
+# def the_same(nums) -> bool:
+#     return len(nums) != len(set(nums))
+#
+#
+# print(the_same([11,12,3,5,6,4]))
+
+
+# def square(nums):
+#     squared = [i**2 for i in nums]
+#     squared.sort()
+#     return squared
+# print(square([2,3,4,4]))
+
+
+# def sum_dict(nums):
+#     return sum(i for i in nums if i % 2 == 0)
+# print(sum_dict([23,4,5,6,7]))
+
+
+from datetime import datetime
+
+class Friend:
+    def __init__(self, name, birth_date):
+        self.name = name
+        self.__birth_date = birth_date  # приватный атрибут
+
+    @property
+    def age(self):
+        today = datetime.today()
+        # Вычисляем возраст по годам
+        years = today.year - self.__birth_date.year
+        # Проверяем, был ли день рождения в этом году
+        if (today.month, today.day) < (self.__birth_date.month, self.__birth_date.day):
+            years -= 1
+        return years
+
+    def introduce(self):
+        return f"My name is {self.name} and I am {self.age} years old."
+
+# Пример использования:
+friend1 = Friend("Maxim", datetime(2000, 2, 20))
+friend2 = Friend("Anna", datetime(1995, 10, 10))
+
+print(friend1.introduce())
+print(friend2.introduce())
+
+# Можно обращаться к age напрямую:
+print(friend1.age)  # → возраст Максим
