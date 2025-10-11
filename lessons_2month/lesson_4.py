116eb234fd14d087a6b6cec356daf86cc8664e61
nuruyql <nurbolsuiunbekov626@gmail.com>
2025-10-11 09:28:10 +0600

    save changed

--- lessons_2month/lesson_4.py
+++ lessons_2month/lesson_4.py
@@ -1,25 +1,86 @@
+# class Animal:
+#     def speak(self):
+#         print("Animal speaking")
+#
+#
+# class Cat(Animal):
+#     def speak(self):
+#         print("мяу")
+#         super().speak()
+#
+# class Dog(Animal):
+#     def speak(self):
+#         print("гав")
+#         super().speak()
+#
+# class CatDog(Dog, Cat):
+#     def speak(self):
+#         print("гав мяу")
+#         super().speak()
+#
+#
+# kotopes = CatDog()
+# kotopes.speak()
+# print(CatDog.__mro__) # methods resolution order
+# print(Dog.__mro__)
+
+
+# from abc import ABC, abstractmethod
+#
+# class Animal(ABC):
+#     @abstractmethod
+#     def make_sound(self):
+#         pass
+#     @abstractmethod
+#     def walk(self):
+#         pass
+#
+#
+# class Dog(Animal):
+#     def make_sound(self):
+#         print("Woof!")
+#
+#     def walk(self):
+#         pass
+# dog1 = Dog()
+# dog1.make_sound()
+
+
 class Animal:
     def speak(self):
-        print("Animal speaking")
-
-
-class Cat(Animal):
-    def speak(self):
-        print("мяу")
-        super().speak()
-
+        print("Hy")
 class Dog(Animal):
     def speak(self):
-        print("гав")
-        super().speak()
-
-class CatDog(Dog, Cat):
+        print("Woof")
+class Cat(Animal):
     def speak(self):
-        print("гав мяу")
-        super().speak()
+        print("Myi")
+class CatDog(Dog,Cat):
+    pass
 
 
-kotopes = CatDog()
-kotopes.speak()
-print(CatDog.__mro__) # methods resolution order
-print(Dog.__mro__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
